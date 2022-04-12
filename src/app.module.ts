1fb293946e4fb8dc4a25be92884932ac352da1a4
Vladimirov SK <vladimirovsk@gmail.com>
2022-04-12 14:22:17 +0200

    modify type connection to mongooste

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 import {LoggerMiddleware} from './middleware/logger.middleware';
 import {ConfigModule} from '@nestjs/config';
 import {MongooseModule} from '@nestjs/mongoose';
@@ -9,30 +9,30 @@ import {ProjectModule} from './project/project.module';
 const environment = process.env.NODE_ENV || 'develop'
 
 @Module({
-  imports: [
-      ProjectModule,
-    ConfigModule.forRoot({
-      envFilePath: `.env.${environment}`,
-      isGlobal: true
-    }),
-    MongooseModule.forRoot(
-        process.env.MONGO_CONNECTION,
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-        }
-    )
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ProjectModule,
+		ConfigModule.forRoot({
+			envFilePath: `.env.${environment}`,
+			isGlobal: true
+		}),
+		MongooseModule.forRoot(
+			process.env.MONGO_CONNECTION as string,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true
+			}
+		)
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 
 export class AppModule implements NestModule {
 
 
-  configure(consumer: MiddlewareConsumer): any {
-    consumer
-        .apply(LoggerMiddleware)
-        .forRoutes('*')
-  }
+	configure(consumer: MiddlewareConsumer): any {
+		consumer
+			.apply(LoggerMiddleware)
+			.forRoutes('*')
+	}
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,8 +1,9 @@
-import {Injectable} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
 import {IProject} from './intefaces/project.interface'
 import {CreateProjectDto} from './dto/create-project.dto';
+
 
 @Injectable()
 export class ProjectService {
@@ -12,13 +13,13 @@ export class ProjectService {
 	) {
 	}
 
-	async create(createUserDto:CreateProjectDto):Promise<IProject> {
+	async create(createUserDto: CreateProjectDto): Promise<IProject> {
 		const createUser = new this.projectModel(createUserDto);
 		return await createUser.save();
 	}
 
-	async find(id:string):Promise<IProject> {
-		return await this.projectModel.findById(id).exec();
+	async find(id: string){
+		return this.projectModel.findById(id).exec();
 	}
 
 	async findAll(): Promise<IProject[]> {
